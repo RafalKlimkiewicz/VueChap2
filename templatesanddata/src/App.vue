cc0e150bd212390b0d0e4ef684e408fe623ea217
Rklim <klimek1008@gmail.com>
2024-10-10 23:51:29 +0200

    Init directives chapter

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-primary text-white text-center m-2 p-3">
-    <h3>Product: {{ name | reverse | capitalize }}</h3>
-    <h3>Product: {{ name | capitalize | reverse }}</h3>
-    <h3>Price: {{ getTotalPrice(lowTaxRate) | currency(3) }}</h3>
-    <h3>Price: {{ getTotalPrice(highTaxRate) | currency }}</h3>
+    <h3>Product: {{ name }}</h3>
   </div>
+  <button v-on:click="handleClick" class="btn btn-primary">
+    Press mee
+  </button>
 </template>
 
 <script>
@@ -12,40 +12,12 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      name: "kama ratunkowa",
-      price: 300,
-      lowTaxRate: 12,
-      highTaxRate: 20
-    }
-  },
-  computed: {
-    lowTotalPrice: function () {
-      return this.getTotalPrice(this.lowTaxRate);
-    },
-    highTotalPrice: function () {
-      return this.getTotalPrice(this.highTaxRate);
+      name: "kamizelafi  ratunkowa"
     }
   },
   methods: {
-    getTotalPrice(taxRate) {
-      return this.price + (this.price * (taxRate / 100));
+    handleClick() {
     }
   },
-  filters: {
-    currency(value, places) {
-      return new Intl.NumberFormat("pl-PL", { 
-        style: "currency", 
-        currency: "PLN", 
-        minimumFractionDigits: places || 2,
-        maximumFractionDigits: places || 2
-      }).format(value);
-    },
-    capitalize(value){
-      return value[0].toUpperCase() + value.slice(1);
-    },
-    reverse(value){
-      return value.split("").reverse().join("");
-    }
-  }
 }
 </script>
