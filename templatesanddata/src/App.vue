f3a73ecafd9aa44f7140ef222d4c5386942f170c
Rafal K. <klimek1008@gmail.com>
2024-10-13 21:47:10 +0200

    vbind class

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white text-center m-2 p-3">
-      <h3 v-show="counter % 2 == 0">Product: {{ name }}</h3>
-      <h3 v-show="counter % 2 != 0">Price: {{ price }}</h3>
+      <h3 v-bind:class="elemClasses" class="display-4">Product: {{ name }}</h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
+      Press mee
+    </button>
+    <button v-on:click="handleClick2" class="btn btn-primary">
       Press mee
     </button>
   </div>
@@ -16,16 +18,26 @@ export default {
   data: function () {
     return {
       name: "Product_Name",
-      price: 275,
-      category: "Category_Type",
-      counter: 0,
+      highlight: false,
+      highlight2: false,
+    }
+  },
+  computed:{
+    elemClasses(){
+      return {
+        "text-dark" : this.highlight,
+        "text-light" : this.highlight2,
+      }
     }
   },
   methods: {
     handleClick() {
-      this.counter++;
+      this.highlight = !this.highlight;
+    },
+    handleClick2(){
+      this.highlight2 = !this.highlight2;
     }
-  },
+  }
 }
 </script>
 <style>
