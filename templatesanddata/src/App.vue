0fd1312115b883175bc92c0123aadf1143b66518
Rafal K. <klimek1008@gmail.com>
2024-10-13 21:17:30 +0200

    if else

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white text-center m-2 p-3">
-      <h3 v-if="showElements">Product: {{ name }}</h3>
-      <h3 v-else>Price: {{ price }}</h3>
+      <h3 v-if="counter % 3 == 0">Product: {{ name }}</h3>
+      <h3 v-else-if="counter % 3 == 1">Price: {{ price }}</h3>
+      <h3 v-else> Category: {{ category }}</h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
       Press mee
@@ -16,15 +17,22 @@ export default {
   data: function () {
     return {
       name: "Product_Name",
-      fragment: `<div class="form-group"> Password <input class="form-control" /> </div>`,
-      price : 275,
-      showElements: true,
+      price: 275,
+      category: "Category_Type",
+      counter: 0,
     }
   },
   methods: {
     handleClick() {
-      this.showElements = !this.showElements;
+      this.counter++;
     }
   },
 }
 </script>
+<style>
+h3:first-child {
+  background-color: aquamarine;
+  padding: 10px;
+  color: black;
+}
+</style>
