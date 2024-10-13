173444b5f5af97292ee27a2dfd203e8cf1803c3e
Rafal K. <klimek1008@gmail.com>
2024-10-13 21:48:23 +0200

    vbind style

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white text-center m-2 p-3">
-      <h3 v-bind:class="elemClasses" class="display-4">Product: {{ name }}</h3>
+      <h3 v-bind:style="elemClasses" class="display-4">Product: {{ name }}</h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
-      Press mee
-    </button>
-    <button v-on:click="handleClick2" class="btn btn-primary">
       Press mee
     </button>
   </div>
@@ -19,23 +16,19 @@ export default {
     return {
       name: "Product_Name",
       highlight: false,
-      highlight2: false,
     }
   },
   computed:{
     elemClasses(){
       return {
-        "text-dark" : this.highlight,
-        "text-light" : this.highlight2,
+        "border" : "5px solid red",
+        "background-color" : this.highlight ? "coral" : "",
       }
     }
   },
   methods: {
     handleClick() {
       this.highlight = !this.highlight;
-    },
-    handleClick2(){
-      this.highlight2 = !this.highlight2;
     }
   }
 }
