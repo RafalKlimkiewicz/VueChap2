d5e44b4f9b92d9b36e9e3bb39bc9fcea2fd39f77
Rafal K. <klimek1008@gmail.com>
2024-10-20 15:38:48 +0200

    bubble faze

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="container-fluid">
-    <h3 class="bg-primary text-white text-center mt-2 p-2">{{ message }}</h3>
-    <table class="table table-sm tabel-striped table-bordered">
-      <tr>
-        <th>Index</th>
-        <th>Name</th>
-        <th>Action</th>
-      </tr>
-      <tr v-for="(name, index) in names" v-bind:key="name">
-        <td>{{ index }}</td>
-        <td>{{ name }}</td>
-        <td>
-          <button class="btn btn-sm bg-primary text-white" v-on="buttonEvents" v-bind:data-name="name">Select</button>
-        </td>
-      </tr>
-    </table>
-
+    <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click="handleClick">
+      Outer element
+      <div id="middle-element" class="bg-secondary p-4" v-on:click="handleClick">
+        Element middle
+        <div id="inner-element" class="bg-info p-4" v-on:click="handleClick">
+          Innet elementr
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +17,7 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      buttonEvents:{
+      buttonEvents: {
         click: this.handleClick,
         mousemove: this.handleMouseEvent,
         mouseleave: this.handleMouseEvent,
@@ -36,17 +29,7 @@ export default {
   },
   methods: {
     handleClick($event) {
-      let name = $event.target.dataset.name;
-      this.message = `Select: ${name}`;
-    },
-    handleMouseEvent($event) {
-      let name = $event.target.dataset.name;
-      if ($event.type == "mousemove") {
-        this.message = `Ruch w ${name} ${this.counter++}`;
-      }else{
-        this.counter = 0;
-        this.message = "Ready!";
-      }
+      console.log(`handleClick target: ${$event.target.id} +  currentTarget:  ${$event.currentTarget.id}`);
     }
   }
 }
