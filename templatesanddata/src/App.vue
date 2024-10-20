c5cabaa07048bd82422e4beda7d8700b1915c2a7
Rafal K. <klimek1008@gmail.com>
2024-10-20 17:12:09 +0200

    ctrl + on key

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="container-fluid">
-    <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click.capture="handleClick"
-      v-on:click="handleClick">
-      Outer element
-      <div id="middle-element" class="bg-secondary p-4" v-on:click.stop="handleClick">
-        Element middle
-        <div id="inner-element" class="bg-info p-4" v-on:mousedown.right="handleClick">
-          Inner element
-        </div>
-      </div>
+    <div class="bg-primary p-4 text-white h3">
+      {{ message }}
     </div>
+    <input class="from-control bg-light" placeholder="Enter text" v-on:keydown.ctrl.exact="handleKey"/>
+
   </div>
 </template>
 
@@ -18,19 +13,12 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      buttonEvents: {
-        click: this.handleClick,
-        mousemove: this.handleMouseEvent,
-        mouseleave: this.handleMouseEvent,
-
-      },
-      message: "Ready",
-      names: ["Kajak", "Lifejet", "Foot ball", "Stadium"]
+      message: "Ready"
     }
   },
   methods: {
-    handleClick($event) {
-      console.log(`handleClick target: ${$event.target.id} +  currentTarget:  ${$event.currentTarget.id}`);
+    handleKey($event) {
+      this.message = $event.key;
     }
   }
 }
