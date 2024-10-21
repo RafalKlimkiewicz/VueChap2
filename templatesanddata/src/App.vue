1cdb6a4277f63fb653325183015358257f1ed6d3
Rafal K. <klimek1008@gmail.com>
2024-10-21 22:40:01 +0200

    select list, checkbox, radio v model bind class

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="container-fluid">
-    <div class="m-2 p-2 text-white" v-bind:class="elemClass">
-      <div>Value: {{ elemClass }}</div>
+    <div class="m-2 p-2 text-white" v-bind:class="dataValue">
+      <div>Value: {{ dataValue }}</div>
     </div>
 
     <div class="form-check m-2">
       <label class="form-check-label">
-        <input type="checkbox" class="form-check-input" v-model="dataValue"/>
-        Dark color
+        <input v-model="dataValue" type="checkbox" v-bind:true-value="darkColor" v-bind:false-value="lightColor"
+          class="form-check-input" />
+        Dark Color
       </label>
     </div>
 
+    <div class="form-group m-2 p-2 bg-secondary">
+      <label>Color</label>
+      <select v-model="dataValue" class="form-control">
+        <option v-bind:value="darkColor">Dark Color</option>
+        <option v-bind:value="lightColor">Light Color</option>
+      </select>
+    </div>
+
+    <div class="form-check-inline m-2">
+      <label class="form-check-label">
+        <input type="radio" class="form-check-input" v-model="dataValue" v-bind:value="darkColor" />
+        Dark Color
+      </label>
+    </div>
+
+    <div class="form-check-inline m-2">
+      <label class="form-check-label">
+        <input type="radio" class="form-check-input" v-model="dataValue" v-bind:value="lightColor" />
+        Light color
+      </label>
+
+    </div>
   </div>
 </template>
 
@@ -19,12 +42,9 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      dataValue: false,
-    }
-  },
-  computed:{
-    elemClass(){
-      return this.dataValue ? "bg-primary" : "bg-info";
+      darkColor: "bg-primary",
+      lightColor: "bg-info",
+      dataValue: "bg-info"
     }
   }
 }
