9ef10a9abaaf997684324fc35f397dd8af423beb
Rafal K. <klimek1008@gmail.com>
2024-10-29 23:14:03 +0100

    props, slot

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="bg-secondary text-white text-center m-2 p-2 h5">
-    {{ message }}
-    <ChildComponenet />
-    <child-componenet />
-    <MyFeature labelText="Name props" initialValue="initial value props"/>
-    <my-feature label-text="Name props" initial-value="initial value props"/>
-    <child-global-component />
+    <product-display v-bind:product="product">
+      <div slot-scope="data" class="bg-info text-left">
+        {{ data.propname }} has value {{  data.propvalue }}
+      </div>
+    </product-display>
+    <h6>{{ message }}</h6>
+    <my-feature v-bind:initial-product="product" v-on:productSubmit="updateProduct" >
+      <div slot="header" class="bg-warning m-2 p-2 h3 text-dark">
+        Edit product
+      </div>
+      <div slot="footer" class="bg-warning p-2 h3 text-dark">
+        Check form before send
+      </div>
+    </my-feature>
   </div>
 </template>
 
 <script>
 import ChildComponenet from "./components/ChildComponent";
+import ProductDisplay from "./components/ProductDisplay.vue";
 export default {
   name: 'App',
   components: {
-    ChildComponenet,
     MyFeature: ChildComponenet,
+    ProductDisplay
   },
-  data: function(){
+  data: function () {
     return {
-      message: "PARENT MESSAGE"
+      message: "This is parent",
+      product: {
+        name: "Kajak",
+        category: "Water sports",
+        price: 275,
+      }
+    }
+  },
+  methods:{
+    updateProduct(newProduct){
+      this.message = JSON.stringify(newProduct);
     }
   }
 }
--- templatesanddata/src/components/ChildComponent.vue
+++ templatesanddata/src/components/ChildComponent.vue
@@ -1,19 +1,38 @@
 <template>
     <div class="bg-primary text-white text-center m-2 p-2 h6">
-        {{ message }}
+        <slot name="header">
+            <h4>Use this form to change data</h4>
+        </slot>
         <div class="form-group m-1 text-left">
-            <label>{{ labelText }}</label>
-            <input v-model="message" class="form-control" />
+            <label>Name</label>
+            <input v-model="product.name" class="form-control" />
+        </div>
+        <div class="form-group m-1 text-left">
+            <label>Category</label>
+            <input v-model="product.category" class="form-control" />
+        </div>
+        <div class="form-group m-1 text-left">
+            <label>Price</label>
+            <input v-model="product.price" class="form-control" />
+        </div>
+        <slot name="footer"></slot>
+        <div class="m-2">
+            <button class="btn btn-info" v-on:click="doSubmit">Send</button>
         </div>
     </div>
 </template>
 
 <script>
-export default{
-    props: ["labelText", "initialValue"],
-    data: function(){
+export default {
+    props: ["initialProduct"],
+    data: function () {
         return {
-            message: this.initialValue
+            product: this.initialProduct || {}
+        }
+    },
+    methods: {
+        doSubmit() {
+            this.$emit("productSubmit", this.product);
         }
     }
 }
