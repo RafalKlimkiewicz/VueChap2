d126eeebf6713317dcb7082338ae60c83e40a12e
Rafal K. <klimek1008@gmail.com>
2024-10-30 21:40:29 +0100

    life cycle project

---
+++ lifecycles/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-primary text-white m-2 p-2">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" v-model="checked" />
+      <label>Checkbox field</label>
+    </div>
+    State of selection: {{ checked }}
+    <div class="bg-info p-2">
+      Names:
+      <ul>
+        <li v-for="name in names" v-bind:key="name">
+          {{ name }}
+        </li>
+      </ul>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      checked: true,
+      names: []
+    }
+  },
+  beforeCreate() {
+    console.log(`beforeCreate(), value: ${this.checked}`);
+  },
+  created() {
+    console.log(`create(), value: ${this.checked}`);
+  }
+}
+</script>
---
+++ lifecycles/vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports={
+  runtimeCompiler : true
+}
+
+module.exports = defineConfig({
+  transpileDependencies: true
+})
