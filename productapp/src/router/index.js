2a8b69fc236749cf1ba9c525e39e96d5f1ce8e8c
Rafal K. <klimek1008@gmail.com>
2024-11-19 23:09:43 +0100

    gaurdian router

--- productapp/src/router/index.js
+++ productapp/src/router/index.js
@@ -1,68 +1,37 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import ProductDisplay from "../components/ProductDisplay";
-import ProductEditor from "../components/ProductEditor";
-import Preferences from "../components/PreferencesComponent";
-import Products from "../components/ProductsComponent";
-import SideBySide from "../components/SideBySide";
+
+import basicRoutes from "./basicRoutes";
+import sideBySideRoutes from "./sideBySideRoutes";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
     mode: "history",
     routes: [
-        {
-            path: "/preferences",
-            component: Preferences
-        },
-        {
-            path: "/products",
-            component: Products,
-            children:
-                [
-                    {
-                        name: "table",
-                        path: "list",
-                        component: ProductDisplay,
-                    },
-                    {
-                        name: "editor",
-                        path: "/:op(create|edit)/:id(\\d+)?",
-                        component: ProductEditor
-                    },
-                    {
-                        path: "",
-                        redirect: "list"
-                    }
-                ]
-        },
-        {
-            path: "/edit/:id",
-            redirect: to => `/products/edit/${to.params.id}`
-        },
-        {
-            path: "/named",
-            component: SideBySide,
-            children:[
-                {
-                    path: "tableleft",
-                    components:{
-                        left: ProductDisplay,
-                        right: ProductEditor
-                    }
-                },
-                {
-                    path: "tableright",
-                    components:{
-                        right: ProductDisplay,
-                        left: ProductEditor
-                    }
-                }
-            ]
-        },
+        ...basicRoutes,
+        sideBySideRoutes,
         {
             path: "*",
             redirect: "/products/list"
         }
     ]
-})
+});
+
+export default router;
+
+router.beforeEach((to, from, next) =>{
+    if(to.path == "/preferences" && from.path.startsWith("/named")){
+        next(false);
+    }else{
+        next();
+    }
+});
+
+router.beforeEach((to, from, next) =>{
+    if(to.path == "/named/tableright"){
+        next("/products");
+    }else{
+        next();
+    }
+});
