ea1872b838774b2f103badcdbbca4de2598e0bc9
Rafal K. <klimek1008@gmail.com>
2024-11-19 21:25:16 +0100

    scoped style exact active

--- productapp/src/App.vue
+++ productapp/src/App.vue
@@ -3,8 +3,10 @@
     <div class="row">
       <div class="col text-center m-2">
         <ol>
-          <router-link tag="li" to="/list" event="mouseenter" >List</router-link>
-          <router-link tag="li" to="/create" event="mouseenter">Create</router-link>
+          <router-link tag="li" to="/list" event="mouseenter">List</router-link>
+          <router-link tag="li" to="/create">Create</router-link>
+          <router-link tag="li" to="/edit" exact>Edit</router-link>
+          <router-link tag="li" to="/edit/1">Edit Kajak</router-link>
         </ol>
       </div>
     </div>
@@ -24,3 +26,24 @@ export default {
   },
 }
 </script>
+<style scoped>
+router-link {
+  text-align: right;
+  color: yellow;
+  background-color: red;
+}
+
+li {
+  text-align: left;
+  color: blue;
+  background-color: lightblue;
+}
+
+.router-link-active {
+  font-size: xx-large;
+}
+
+.router-link-exact-active {
+  font-weight: bolder;
+}
+</style>
