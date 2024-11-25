f6c58ba58e9c06434fd030e70b5c8d0e688b23d7
Rafal K. <klimek1008@gmail.com>
2024-11-25 23:41:05 +0100

    vue extensions directives

---
+++ extendingvue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-2">
+    <numbers />
+  </div>
+</template>
+
+<script>
+import Numbers from "./components/NumbersDisplay.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Numbers
+  }
+}
+</script>
