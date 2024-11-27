43162caa1062bdcdc818bd26b06da7f1dc426463
Rafal K. <klimek1008@gmail.com>
2024-11-28 00:17:24 +0100

    custom optional after dot

--- extendingvue/src/directives/colorize.js
+++ extendingvue/src/directives/colorize.js
@@ -1,9 +1,13 @@
 export default {
-    update(el, binding){
+    update(el, binding) {
         const bgClass = binding.arg || "bg-danger";
-        if(binding.value){
-            el.classList.add(bgClass, "text-white");
-        }else{
+        const noMods = Object.keys(binding.modifiers).length == 0;
+        if (binding.value) {
+            if (noMods || binding.modifiers.bg)
+                el.classList.add(bgClass);
+            if (noMods || binding.modifiers.text)
+                el.classList.add("text-white");
+        } else {
             el.classList.remove(bgClass, "text-white");
         }
     }
